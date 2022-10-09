9028a8d92baf2d715b6a3babe0172b5d661bace8
heyzec <61238538+heyzec@users.noreply.github.com>
2022-10-10 00:54:25 +0800

    feat: make Tournament accept Player eval scores

--- player.py
+++ player.py
@@ -7,7 +7,11 @@ pipe_send_user_action = Pipe(PIPE_FILE_SEND_USER_ACTION, 'o')
 
 class Player(abc.ABC):
     @abc.abstractmethod
-    def make_move(self, board):
+    def make_move(self, board) -> list:
+        raise NotImplementedError()
+
+    @abc.abstractmethod
+    def make_move_with_eval(self, board) -> tuple[list, float]:
         raise NotImplementedError()
 
 class PlayerManual(Player):
@@ -17,3 +21,6 @@ class PlayerManual(Player):
             print("OH NO")
             return
         return data
+
+    def make_move_with_eval(self, board) -> tuple[list, float]:
+        return self.make_move(board), 0
--- tournament.py
+++ tournament.py
@@ -11,50 +11,64 @@ pipe_board_updates = Pipe(PIPE_FILE_BOARD_UPDATES, 'o')
 
 
 class Tournament:
-    def __init__(self, player1: Player, player2: Player, initial_board=None, show_board=False):
+    def __init__(self, black: Player, white: Player, initial_board=None, show_board=False):
         if initial_board is None:
             initial_board = Board.generate_init_board()
 
         self.move = 0
         self.board: Board = initial_board
-        self.player1 = player1
-        self.player2 = player2
+        self.black = black
+        self.white = white
+        self.black_eval = None
+        self.white_eval = None
         self.show_board = show_board
+        self.winner = None
         
     def is_black_turn(self):
         return self.move % 2 == 0
     
 
-    def play(self):
-        while not self.board.game_over():
-            if self.is_black_turn():
-                player = self.player1
-            else:
-                player = self.player2
-            
-            if not self.is_black_turn():
-                self.board = self.board.invert()
-            move = player.make_move(self.board.board)
-            src, dst = move
-            # makes the move effective on the board
-            self.board = self.board.move(src, dst)
-            if not self.is_black_turn():
-                self.board = self.board.invert()
+    def play_once(self):
+        assert not self.board.game_over()
+        if self.is_black_turn():
+            player = self.black
+        else:
+            player = self.white
+        
+        if not self.is_black_turn():
+            self.board = self.board.invert()
+        move, val = player.make_move_with_eval(self.board.board)
+        src, dst = move
+        # makes the move effective on the board
+        self.board = self.board.move(src, dst)
+        if not self.is_black_turn():
+            self.board = self.board.invert()
 
-            colour = "someone"
-            print(f'Move No: {move} by {type(player).__name__}')
-            if self.show_board:
-                # printing the current configuration of the board after making move
-                utils.print_state(self.board)
-            pipe_board_updates.write(self.board.board)
-            self.move += 1
+        colour = "someone"
+        print(f'Move No: {move} by {type(player).__name__}')
+        if self.show_board:
+            # printing the current configuration of the board after making move
+            utils.print_state(self.board)
+        pipe_board_updates.write(self.board.board)
+        self.move += 1
+        
+        if self.board.game_over():
+            print("GAME OVER")
+            self.winner = player
+        return move, val
+        
+    def play_until_game_over(self):
+        while True:
+            self.play_once()
+            if self.winner is not None:
+                break
             
         
 def main():
     # tournament = Tournament(PlayerManual(), PlayerAI())
     tournament = Tournament(PlayerAI(), PlayerManual())
     tournament = Tournament(PlayerAI(), PlayerAI())
-    tournament.play()
+    tournament.play_until_game_over()
     
 if __name__ == '__main__':
     main()
